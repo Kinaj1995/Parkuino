c51853bed1e4d77630968639d0240df4473bba1d
unknown <puchtuningproduction@gmail.com>
2022-07-23 15:38:10 +0200

    Upload working

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
 #include <Arduino.h>
 
-void setup() {
+int loopcount = 0;
+
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
+  while (!Serial)
+  {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  
   Serial.println("Starting");
-  for(int i; i < 10; i++){
+  for (int i = 0; i < 10; i++)
+  {
     Serial.print(".");
   }
 
   Serial.println(".");
-
-  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  loopcount++;
+
+  Serial.println(loopcount);
+  delay(100);
 }
